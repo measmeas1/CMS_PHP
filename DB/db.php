a0a9244cf3c11348503da8f00f10c09abc42faa1
measmeas1 <mabmeas12@gmail.com>
2025-03-15 14:49:29 +0700

    make dtabase and database connection

---
+++ DB/db.php
@@ -0,0 +1,13 @@
+<?php 
+  $server = 'localhost';
+  $user = 'root';
+  $pass = '';
+  $db = 'cms_php';
+  $port = 3307;
+
+  $con = mysqli_connect($server, $user, $pass, $db, $port);
+  
+  if (!$con){
+    die("Connection failed: ". mysqli_connect_error());
+  }
+?> 
